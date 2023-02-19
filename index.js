b380892002213b21dd555a21db3095e9546b51db
Chingis007 <karaulnekerell@gmail.com>
2023-02-19 11:58:56 +0200

    l

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const port = process.env.PORT || 7544;
 const db = require("./models");
 
 //Routers
+const usersRouter = require("./routes/Users");
+app.use("/auth", usersRouter);
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
 const commentsRouter = require("./routes/Comments");
 app.use("/comments", commentsRouter);
-const usersRouter = require("./routes/Users");
-app.use("/auth", usersRouter);
 const likesRouter = require("./routes/Likes");
 app.use("/likes", likesRouter);
 console.log("starting sequelize.sinc()");
